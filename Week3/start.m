d4742d4eb0070011aa60431491b0d9aed11e5a5b
Eloi Bove <eloibove@gmail.com>
2020-11-05 13:19:54 +0100

    Added image and regularized initialization

--- Week3/start.m
+++ Week3/start.m
@@ -10,6 +10,8 @@ clc
 I=double(imread('phantom18.bmp'));
 %I=double(imread('phantom19.bmp'));
 %I=double(imread('Image_to_Restore.png'));
+%I=double(imread('che.jpg'));
+
 I=mean(I,3);
 I=I-min(I(:));
 I=I/max(I(:));
@@ -24,7 +26,7 @@ I=I/max(I(:));
 %phantom17 mu=1, mu=2, mu=10
 %phantom18 mu=0.2 mu=0.5
 %hola carola mu=1
-mu=0.2;
+mu=0.1;
 nu=0;
 
 
@@ -41,17 +43,19 @@ tol=0.000001;
 %dt=(10^-2)/mu; 
 dt=(10^-1)/mu;
 
-iterMax=100000
+iterMax=300
 %reIni=100; %Try both of them
 %reIni=500;
 reIni=0;
 [X, Y]=meshgrid(1:nj, 1:ni);
 
 %%Initial phi
-%phi_0=(-sqrt( ( X-round(ni/2)).^2 + (Y-round(nj/2)).^2)+50);
+phi_0=(-sqrt( ( X-round(ni/2)).^2 + (Y-round(nj/2)).^2)+50);
+%%Initial phi: regularized
+%phi_0= sin(pi/5 * X) .* sin(pi/5 * Y);
 
 %%% This initialization allows a faster convergence for phantom 18
-phi_0=(-sqrt( ( X-round(ni/2)).^2 + (Y-round(nj/4)).^2)+50);
+%phi_0=(-sqrt( ( X-round(ni/2)).^2 + (Y-round(nj/4)).^2)+50);
 % Normalization of the initial phi to [-1 1]
 % phi_0=phi_0-min(phi_0(:));
 % phi_0=2*phi_0/max(phi_0(:));
@@ -67,7 +71,7 @@ phi_0=phi_0-1;
 %%Explicit Gradient Descent
 
 %Plot every vis iterations 
-vis = 100;
+vis = 0;
 %output as .avi file
 vid = false;
 phi=sol_ChanVeseIpol_GDExp( I, phi_0, mu, nu, eta, lambda1, lambda2, tol, epHeaviside, dt, iterMax, reIni, vis, vid );
