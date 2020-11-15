fbe7795e9569d4d9c0bd4400d4cb33089e5c769e
Arnau Roche <arnau.roche@gmail.com>
2020-11-15 21:02:29 +0100

    New inference methods

--- LowLevelSegmentation/CreateGridUGMModel.m
+++ LowLevelSegmentation/CreateGridUGMModel.m
@@ -13,7 +13,7 @@ tic
 nNodes = NumFils*NumCols;
 nStates = K;
 
-% Create sparse matrix
+% Create sparse matrix (defining the nodes and their connections)
 adj = sparse(nNodes,nNodes);
 
 % Add Down Edges
@@ -37,19 +37,27 @@ edgeStruct = UGM_makeEdgeStruct(adj,nStates);
 % Standardize Features
 Xstd = UGM_standardizeCols(reshape(im,[1 1 nNodes]),1);
 
-% adj is a sparse matrix defining the nodes and their connections
-
 % Define the pairwise potentials (Potts model)
 edgePot = zeros(nStates,nStates,edgeStruct.nEdges);
 
 for e = 1:edgeStruct.nEdges 
+   % Option 1: pot_same in the diagonal and 1 elsewhere
    n1 = edgeStruct.edgeEnds(e,1);
    n2 = edgeStruct.edgeEnds(e,2);
 
    pot_same = exp(lambda(1) + lambda(2)*1/(1+abs(Xstd(n1)-Xstd(n2))));
    edgePot(:,:,e) = (pot_same)*eye(K)+(ones(K)-eye(K));
-   %pot_same in the diagonal and 1 elsewhere
+   
+   % Option 2: use directly lambda(1) for the diagonal and lambda(2) for
+   % the rest
+   %edgePot(:,:,e) = (exp(1+lambda(1)))*eye(K)+(exp(1+lambda(2)))*(ones(K)-eye(K));
+   
+   % Option 3: change manually values (for K=3)
+   %edgePot(:,:,e) =  [1     1       1;
+                      %1     1       1;
+                      %1     1       4000];
 end
 
+edgePot(3,:,:) = edgePot(3,:,:).*100;
 
 toc;
--- LowLevelSegmentation/main.m
+++ LowLevelSegmentation/main.m
@@ -8,7 +8,7 @@ addpath(genpath(basedir))
 % Input image
 imname = '7_9_s';
 lab = true;
-K = 3;
+K = 3; % Number of color clusters (=number of states of hidden variables)
 smooth_term = [5 2.5];
 
 im = imread(strcat(imname,'.bmp'));
@@ -26,8 +26,11 @@ gmm_color = gmdistribution.fit(x,K);
 mu_color=gmm_color.mu;
 
 %% Estimate Unary potentials (GMM)
-data_term=gmm_color.posterior(x);
-[~,c] = max(data_term,[],2);
+data_term=gmm_color.posterior(x); % Posterior probabilities
+                                  % ROWS: each of the pixels
+                                  % COLS: each of the color clusters
+[~,c] = max(data_term,[],2); % Take the index (=color cluster) of the
+                             % maximum probability for each pixel
 
 %% Estimate Pairwise potentials
 x1 = reshape(rgb2gray(im/255),[nRows*nCols,1]);
@@ -46,21 +49,36 @@ im_lbp = reshape(mu_color(im_lbp,:),size(im)); %Use the color with max prob
 decodeLBP = UGM_Decode_LBP(data_term,edgePot,edgeStruct);
 im_bp = reshape(mu_color(decodeLBP,:),size(im));
 
+% Extra inference algorithms: MeanField
+[nodeBelMeanField, ~, ~] = UGM_Infer_MeanField(data_term,edgePot,edgeStruct);
+[~,im_mf] = max(nodeBelMeanField,[],2); % Take the index (=color cluster) of the
+                                         % maximum probability for each pixel
+im_mf = reshape(mu_color(im_mf,:),size(im)); %Use the color with max prob
+
+% Extra inference algorithms: Chain
+[nodeBelChain, ~, ~] = UGM_Infer_Chain(data_term,edgePot,edgeStruct)
+[~,im_chain] = max(nodeBelChain,[],2); % Take the index (=color cluster) of the
+                                         % maximum probability for each pixel
+im_chain = reshape(mu_color(im_chain,:),size(im)); %Use the color with max prob
 %%
 % Plotting
 figure
 if lab
-    subplot(2,3,1), imshow(Lab2RGB(im)); xlabel('Original') ;
-    subplot(2,3,2), imshow(Lab2RGB(im_lbp)); xlabel('Infer LBP') ;
-    subplot(2,3,3), imshow(Lab2RGB(im_bp)); xlabel('Decode LBP') ;
-    subplot(2,3,4), imshow(Lab2RGB(im_c)); xlabel('GMM') ;
-    subplot(2,3,5), imshow(im_gt); xlabel('GT') ;
+    subplot(2,4,1), imshow(Lab2RGB(im)); xlabel('Original') ;
+    subplot(2,4,2), imshow(Lab2RGB(im_lbp)); xlabel('Infer LBP') ;
+    subplot(2,4,3), imshow(Lab2RGB(im_bp)); xlabel('Decode LBP') ;
+    subplot(2,4,4), imshow(Lab2RGB(im_c)); xlabel('GMM') ;
+    subplot(2,4,5), imshow(Lab2RGB(im_mf)); xlabel('Infer Mean Field') ;
+    subplot(2,4,6), imshow(Lab2RGB(im_chain)); xlabel('Infer Chain') ;
+    subplot(2,4,7), imshow(im_gt); xlabel('GT') ;
 else
-    subplot(2,3,1), imshow(im/255); xlabel('Original') ;
-    subplot(2,3,2), imshow(im_lbp/255); xlabel('Infer LBP') ;
-    subplot(2,3,3), imshow(im_bp/255); xlabel('Decode LBP') ;
-    subplot(2,3,4), imshow(im_c/255); xlabel('GMM') ;
-    subplot(2,3,5), imshow(im_gt); xlabel('GT') ;
+    subplot(2,4,1), imshow(im/255); xlabel('Original') ;
+    subplot(2,4,2), imshow(im_lbp/255); xlabel('Infer LBP') ;
+    subplot(2,4,3), imshow(im_bp/255); xlabel('Decode LBP') ;
+    subplot(2,4,4), imshow(im_c/255); xlabel('GMM') ;
+    subplot(2,4,5), imshow(im_mf/255); xlabel('Infer Mean Field') ;
+    subplot(2,4,6), imshow(im_chain/255); xlabel('Infer Chain') ;
+    subplot(2,4,7), imshow(im_gt); xlabel('GT') ;
 end
 
 
